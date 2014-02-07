6490ba574e0c40703f7f17a9d17dceb8f2a4d14e
Adam Simpson <adam@heysparkbox.com>
2014-02-06 21:54:26 -0500

    feat: add message after install is complete with next steps

--- app/index.js
+++ app/index.js
@@ -2,13 +2,21 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-
+var chalk = require('chalk');
 
 var MgGenerator = module.exports = function MgGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+
+    this.installDependencies({ 
+      skipInstall: options['skip-install'],
+      callback: function () {
+        console.log(chalk.red("Run ") + chalk.bold.yellow("'grunt'") + chalk.red(" to build out your assets"));
+        console.log(chalk.red("Run ") + chalk.bold.yellow("'grunt server'") + chalk.red(" to spin up a local web server to view your project"));
+      }
+    });
+
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
